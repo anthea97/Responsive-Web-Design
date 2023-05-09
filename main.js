eafda03575218d82009b160971d860ba349d03e8
Anthea Abreo <32321847+anthea97@users.noreply.github.com>
2023-05-09 12:32:31 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+//Image Switcher
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.jpeg") {
+    myImage.setAttribute("src", "images/firefox2.jpeg");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.jpeg");
+  }
+};
+
+//Welcome Message
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if(!myName){
+        setUserName();
+    }
+    else{
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+}
+
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Welcome, ${storedName}`;
+  }
+
+  myButton.onclick = () => {
+    setUserName();
+  };
